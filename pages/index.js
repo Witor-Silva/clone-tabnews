76b764030478b0db04b475b75aaaea3ce7d7140a
Vitor Silva <108163445+Witor-Silva@users.noreply.github.com>
2024-03-11 01:19:16 +0000

    Changed the phrase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Obrigado por acreditar em mim! Sou eternamente grato por tudo que você fez por mim e espero retribuir todo esse apoio e confiança que você deposita em mim!</h1>
+    return <h1>Obrigado por acreditar em mim! Sou eternamente grato por tudo que você fez e espero retribuir todo esse apoio e confiança que você deposita em mim!</h1>
 }
 
 export default Home;
