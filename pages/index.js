681dbe33d313dc809f2dc0755831e26878305689
Vitor Silva <108163445+Witor-Silva@users.noreply.github.com>
2024-03-25 01:43:12 +0000

    Adiciona scripts  e

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Luana, se você me ama, da uma um coraçãozinho coreano ❤️</h1>
+  return <h1>Luana, se você me ama, da uma um coraçãozinho coreano ❤️</h1>;
 }
 
 export default Home;
