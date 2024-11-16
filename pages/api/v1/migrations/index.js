f0cc68d26c73237d5421f4d57e6c7760f2cd4f73
Vitor Silva <108163445+Witor-Silva@users.noreply.github.com>
2024-11-16 21:27:11 +0000

    migrations tests

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,16 +3,32 @@ import { join } from "node:path";
 
 export default async function migrations(request, response) {
 
-  const migrations = await migrationRunner({
+  if (request.method === 'GET') {
+    const migrations = await migrationRunner({
 
-    databaseUrl: process.env.DATABASE_URL,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  })
-  response.status(200).json(migrations);
+      databaseUrl: process.env.DATABASE_URL,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    })
+    return response.status(200).json(migrations);
+  }
 
+  if (request.method === 'POST') {
+    const migrations = await migrationRunner({
+
+      databaseUrl: process.env.DATABASE_URL,
+      dryRun: false,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    })
+    return response.status(200).json(migrations);
+  }
+
+  return response.status(405);
 }
 
