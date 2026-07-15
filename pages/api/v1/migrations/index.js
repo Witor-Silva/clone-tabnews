e2aef96139cb343d495a94cf598b6bc93f42e10a
Vitor Silva <108163445+Witor-Silva@users.noreply.github.com>
2026-07-15 17:57:00 +0000

    refactor: pendding migration to migrated migrated

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,30 +3,31 @@ import { join } from "node:path";
 
 export default async function migrations(request, response) {
 
-  if (request.method === 'GET') {
-    const migrations = await migrationRunner({
+  const defaultMigrations = {
 
-      databaseUrl: process.env.DATABASE_URL,
-      dryRun: true,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
-    })
-    return response.status(200).json(migrations);
+    databaseUrl: process.env.DATABASE_URL,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  }
+
+  if (request.method === 'GET') {
+    const pendingMigrations = await migrationRunner(defaultMigrations)
+    return response.status(200).json(pendingMigrations);
   }
 
   if (request.method === 'POST') {
-    const migrations = await migrationRunner({
-
-      databaseUrl: process.env.DATABASE_URL,
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrations,
       dryRun: false,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
-    })
-    return response.status(200).json(migrations);
+    });
+
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
+    }
+    return response.status(200).json(migratedMigrations);
   }
 
   return response.status(405);
